9c719a7bfe22c48aa985213e1baff1ce194e9a60
ock2023 <120083605+ock2023@users.noreply.github.com>
2024-02-09 22:25:35 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.js
@@ -0,0 +1,10 @@
+function solution(age) {
+    // 2022 - 40 = 1982
+    // 2022 - 23 = 1999
+    // age = (2022 - age) + 1 
+    // var answer = 0;
+    // let today = new Date();
+    // answer =  today.getFullYear() - age + 1;
+    // return answer;
+    return 2022 - age + 1;
+}
